afb049e5e85fdd6515dc81b721a5463451bd4237
Rohan Bhardwaj <122192870+RohanBhardwaj10@users.noreply.github.com>
2025-07-16 22:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from langchain_core.prompts import ChatPromptTemplate
 import os
 from dotenv import load_dotenv
 load_dotenv()
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = "My ollama Q&A chatbot "
 
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "My ollama Q&A chatbo
 # Prompt Template
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful friend. Please respond to the user."),
